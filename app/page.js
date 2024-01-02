8cc20ff41e901720481376033b31ccc38bc3bbc0
user.name:"Amir Shoeibi Omrani <amirtahaomrany@gmail.com>
2024-01-02 17:06:32 -0600

    >>>Contacts

--- app/page.js
+++ app/page.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
-// import Contacts from "./components/Contacts";
-// import ContactsList from "./components/ContactsList";
+import Contacts from "./components/Contacts";
+import ContactsList from "./components/ContactsList";
 
 function HomePage() {
   return (
     <div>
       <Header />
-      {/* <Contacts /> */}
-      {/* <ContactsList /> */}
+      <Contacts />
+      <ContactsList />
     </div>
   );
 }
